e6faff6bf918fa91736843b407a43d85c7b2b281
zack-guo <52080300+zack-guo@users.noreply.github.com>
2019-12-31 13:45:23 +0800

    0.0

---
+++ sudoku/sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sudoku
+//
+//  Created by 郭子奇 on 2019/12/31.
+//  Copyright © 2019 郭子奇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
